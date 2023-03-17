b68181289a403ee5878c3db460346bc0c7397055
Ariesta <ikadekagusariestaputra@gmail.com>
2023-03-17 23:32:35 +0800

    Repair headers value

--- WebPageClone/__init__.py
+++ WebPageClone/__init__.py
@@ -66,9 +66,11 @@ def get_content(url, max_retry=2):
     if max_retry == 0: return None
     try:
         headers = {
-            'accept-language': 'en-US,en;q=0.9,id;q=0.8',
-            'cache-control': 'max-age=0',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'no-cache',
             'dnt': '1',
+            'pragma': 'no-cache',
             'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
@@ -79,7 +81,7 @@ def get_content(url, max_retry=2):
             'upgrade-insecure-requests': '1',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
         }
-        req = requests.get(url, headers=headers, timeout=5, allow_redirects=False, verify=False)
+        req = requests.get(url, headers=headers, timeout=5, verify=False)
         return req
     except Exception as ex:
         logging.error("{} >> {}", url, ex)
